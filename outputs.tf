58410fa2b5d21a2494ba6d03f5ebb7dc95ff460e
Nemo <commits@captnemo.in>
2023-12-15 13:40:51 +0530

    v1.3.0
    - Added new outputs for various regions
    - New regions added: UK/India

--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,107 @@
 output "cidrs" {
   description = "List of all Duo Service CIDRs. Allow for egress to Duo"
 
-  value = [
-    "54.241.191.128/26",
-    "54.236.251.192/26",
-    "52.19.127.192/26",
-    "52.32.63.128/26",
-    "52.59.243.192/26",
-    "35.182.14.128/26",
-    "3.25.48.128/26",
-    "35.74.77.64/26",
-    "13.213.75.128/26"
-  ]
+  value = concat(
+    local.us-cidrs,
+    local.emea-cidrs,
+    local.central-europe-cidrs,
+    local.canada-cidrs,
+    local.australia-cidrs,
+    local.japan-cidrs,
+    local.southeast-asia-cidrs,
+    local.india-cidrs,
+    local.uk-cidrs,
+  )
+}
+
+output "us-cidrs" {
+  description = "List of Duo's Service CIDRs for US deployments"
+  value       = local.us-cidrs
+}
+output "emea-cidrs" {
+  description = "List of Duo's Service CIDRs for EMEA deployments"
+  value       = local.emea-cidrs
+}
+output "central-europe-cidrs" {
+  description = "List of Duo's Service CIDRs for central-europe deployments"
+  value       = local.central-europe-cidrs
+}
+output "canada-cidrs" {
+  description = "List of Duo's Service CIDRs for canada deployments"
+  value       = local.canada-cidrs
+}
+output "australia-cidrs" {
+  description = "List of Duo's Service CIDRs for australia deployments"
+  value       = local.australia-cidrs
+}
+output "japan-cidrs" {
+  description = "List of Duo's Service CIDRs for japan deployments"
+  value       = local.japan-cidrs
+}
+output "southeast-asia-cidrs" {
+  description = "List of Duo's Service CIDRs for southeast-asia deployments"
+  value       = local.southeast-asia-cidrs
+}
+output "india-cidrs" {
+  description = "List of Duo's Service CIDRs for india deployments"
+  value       = local.india-cidrs
+}
+output "uk-cidrs" {
+  description = "List of Duo's Service CIDRs for UK deployments"
+  value       = local.uk-cidrs
 }
 
 output "trusted_endpoints_cidrs" {
   description = "List of Duo's Trusted Endpoint CIDRs. Allow for ingress from Duo"
 
-  value = [
-    "13.56.32.240/29",
-    "52.32.63.176/30",
-    "52.19.127.200/30",
-    "52.59.243.200/30",
-    "35.182.14.128/30",
-    "3.24.48.188/30",
-    "35.74.77.124/30",
-    "13.213.75.172/30"
-  ]
+  value = concat(
+    local.trusted-endpoint-us-cidrs,
+    local.trusted-endpoint-emea-cidrs,
+    local.trusted-endpoint-central-europe-cidrs,
+    local.trusted-endpoint-canada-cidrs,
+    local.trusted-endpoint-australia-cidrs,
+    local.trusted-endpoint-japan-cidrs,
+    local.trusted-endpoint-southeast-asia-cidrs,
+    local.trusted-endpoint-india-cidrs,
+    local.trusted-endpoint-uk-cidrs,
+  )
+}
+
+output "trusted-endpoint-us-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for US Deployments"
+  value       = local.trusted-endpoint-us-cidrs
+}
+output "trusted-endpoint-emea-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for EMEA Deployments"
+  value       = local.a-trusted-endpoint-emea-cidrs
+}
+output "trusted-endpoint-central-europe-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for Central Europe Deployments"
+  value       = local.ope-trusted-endpoint-central-europe-cidrs
+}
+output "trusted-endpoint-canada-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for Canda Deployments"
+  value       = local.ada-trusted-endpoint-canada-cidrs
+}
+output "trusted-endpoint-australia-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for Australia Deployments"
+  value       = local.tralia-trusted-endpoint-australia-cidrs
+}
+output "trusted-endpoint-japan-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for Japan Deployments"
+  value       = local.an-trusted-endpoint-japan-cidrs
+}
+output "trusted-endpoint-southeast-asia-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for Southeast Asia Deployments"
+  value       = local.a-trusted-endpoint-southeast-asia-cidrs
+}
+output "trusted-endpoint-india-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for India Deployments"
+  value       = local.ia-trusted-endpoint-india-cidrs
+}
+output "trusted-endpoint-uk-cidrs" {
+  description = "List of Duo's Trusted Endpoint CIDRs for UK Deployments"
+  value       = local.trusted-endpoint-uk-cidrs
 }
 
 output "ad_hostnames_ca" {
@@ -50,4 +125,24 @@ output "ad_hostnames_us" {
 output "ad_hostnames_au" {
   description = "Map of Duo's Microsoft Azure Active Directory Conditional Access application for AU deployments"
   values      = ["ase2.azureauth.duosecurity.com"]
+}
+
+output "ad_hostnames_asean" {
+  description = "Map of Duo's Microsoft Azure Active Directory Conditional Access application for ASEAN deployments"
+  values      = ["ase1.azureauth.duosecurity.com"]
+}
+
+output "ad_hostnames_jp" {
+  description = "Map of Duo's Microsoft Azure Active Directory Conditional Access application for Japan deployments"
+  values      = ["ane1.azureauth.duosecurity.com"]
+}
+
+output "ad_hostnames_uk" {
+  description = "Map of Duo's Microsoft Azure Active Directory Conditional Access application for UK deployments"
+  values      = ["ew2.azureauth.duosecurity.com"]
+}
+
+output "ad_hostnames_in" {
+  description = "Map of Duo's Microsoft Azure Active Directory Conditional Access application for IN deployments"
+  values      = ["as1.azureauth.duosecurity.com"]
 }
